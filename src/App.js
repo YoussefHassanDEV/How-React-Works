98075ce9f9ae70d6bd0947599d37b3b48cd3f7be
Youssef Hassan <devyoussefhassan@gmail.com>
2024-10-22 22:25:42 +0300

    added handleUndoAfter 3 sec

--- src/App.js
+++ src/App.js
@@ -75,6 +75,9 @@ function TabContent({ item }) {
     setShowDetails(true);
     setLikes(0)
   }
+  function handleUndoLater() {
+    setTimeout(handleUndo,2000)
+  }
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -94,7 +97,7 @@ function TabContent({ item }) {
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
